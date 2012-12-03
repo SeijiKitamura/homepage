bede7a8ec2492b9b7f02d2f1e641280f0b6eebbb
kennpin1 <seiji.kitamura@gmail.com>
2012-12-03 17:09:15 +0900

    tirasi.class/php 修正
    TB_SALEITEMSテーブルからデータを抽出するように大幅に見直し。
    なお、旧クラスはtirasi.class.php.oldで保存中。
    
    next:
    calendar.class.php の修正。
    同様にTB_SALEITEMSからデータを抽出できるようにする。
    
    On branch 20121202
    modified:   php/tirasi.class.php
    renamed:    php/tirasi.class.php -> php/tirasi.class.php.old

--- php/test.php
+++ php/test.php
@@ -1,16 +1,42 @@
+<html>
+<head>
+ <meta charset="utf-8">
+</head>
+<body>
 <?php
-require_once("maillist.class.php");
+require_once("tirasi.class.php");
 try{
  //インスタンス
  echo "success";
- $db=new MAILLIST();
- echo "success2";
- $db->getItemList(1,$_GET["hiduke"]);
+ $db=new TIRASI();
+ $db->flg0=734;
+ $db->saleday="2012/11/29";
  echo "<pre>";
+
+ $db->getJanItem(4979754557176);
  print_r($db->items);
+
+ $db->getClsItem(10108);
+ print_r($db->items);
+
+ $db->getLinItem(1);
+ print_r($db->items);
+
+ $db->getLinList();
+ print_r($db->items);
+
+ $db->getClsList(1);
+ print_r($db->items);
+
+ $db->getItemList();
+ print_r($db->items);
+
  echo "</pre>";
+ echo "success2";
 } 
 catch(Exception $e){
  echo "エラー:".$e->getMessage();
 }
 ?>
+</body>
+</html>
